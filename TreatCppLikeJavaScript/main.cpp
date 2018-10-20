21f08221e319375fd1a82a3a261e8d56e43cbc8c
kungfooman <lama12345@gmail.com>
2018-10-20 11:29:02 +0200

    getter/setter for Bitmap.alpha

--- TreatCppLikeJavaScript/main.cpp
+++ TreatCppLikeJavaScript/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <iostream>
 #include <memory>
 #include <vector>
 
@@ -11,6 +12,13 @@ struct Bitmap {
 	uint8_t& operator[](const int &i) {
 		return (*pixels)[i];
 	}
+
+    class {
+        int value;
+        public:
+            int & operator = (const int &i) { return value = i; }
+            operator int () const { return value; }
+    } alpha;
 
 	const int w;
 	const int h;
@@ -26,7 +34,8 @@ void fill(Bitmap bm, uint32_t color) {
 int main() {
 	Bitmap bitmap(16, 16);
 	fill(bitmap, 0x33);
-	printf("width: %d height: %d\n", bitmap.w, bitmap.h);
+	bitmap.alpha = 5;
+	printf("width: %d height: %d alpha=%d\n", bitmap.w, bitmap.h, bitmap.alpha);
 	getchar();
 	return 0;
 }
