c722a7816c56916601a2177a0084ff9fa921e45e
kungfooman <lama12345@gmail.com>
2018-10-23 18:20:04 +0200

    SetFocus stuff for Z/Cam Wnd and remove unneeded methods

--- q3radiant/ZWnd.h
+++ q3radiant/ZWnd.h
@@ -24,14 +24,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 class CZWnd : public CWnd { public:
 	DECLARE_DYNCREATE(CZWnd);
-	CZWnd();
 	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
-	virtual ~CZWnd();
 	HDC m_dcZ;
 	HGLRC m_hglrcZ;
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
-	afx_msg void OnDestroy();
-	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnMButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
@@ -39,13 +35,8 @@ class CZWnd : public CWnd { public:
 	afx_msg void OnGetMinMaxInfo(MINMAXINFO FAR* lpMMI);
 	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
 	afx_msg void OnSize(UINT nType, int cx, int cy);
-	afx_msg void OnNcCalcSize(BOOL bCalcValidRects, NCCALCSIZE_PARAMS FAR* lpncsp);
-	afx_msg void OnKillFocus(CWnd* pNewWnd);
-	afx_msg void OnSetFocus(CWnd* pOldWnd);
-	afx_msg void OnClose();
 	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
 	afx_msg void OnMButtonUp(UINT nFlags, CPoint point);
 	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
-	afx_msg void OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags);
 	DECLARE_MESSAGE_MAP()
 };
