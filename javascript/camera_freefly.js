5999c53f31ec7b189bb3d75df3a5644e8a6ae284
kungfooman <lama12345@gmail.com>
2018-10-26 19:30:32 +0200

    cam fly mostly works

--- javascript/camera_freefly.js
+++ javascript/camera_freefly.js
@@ -20,6 +20,7 @@ freefly_on = false;
 
 freefly_start = function() {
 	freefly_on = true;
+	oldmousepos = mouse.position;
 	console.log("FREEFLY");
 }
 
@@ -27,15 +28,26 @@ freefly_stop = function() {
 	freefly_on = false;
 }
 
-
 freefly_update = function() {
 	if ( ! freefly_on)
 		return;
+	
+	
 	
 	// do thread stuff etc. later
 	var newDir = getNewDir(camera);
 	var campos = camera.origin;
 	campos.add(newDir);
 	camera.origin = campos;
+	
+	
+	// update angles from mouse pos
+	var deltaMousePos = new pc.Vec2().sub2(mouse.position, oldmousepos );
+	console.log("deltaMousePos", deltaMousePos)
+	deltaMousePos.scale(1/5.333) // 1920 / 360 = 5.333
+	camera.angles = camera.angles.add(new pc.Vec3( -deltaMousePos.y, -deltaMousePos.x, 0)) 
+	mouse.position = oldmousepos; // reset mouse pos
+	
+	
 	Sys_UpdateWindows()
 }
